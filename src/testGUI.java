976e1c1477d876783afe8e22bd15c6562ca587af
Zahin <Zaheen.kenji@gmail.com>
2023-04-06 03:17:00 +0600

    Added program "testGUI"

---
+++ src/testGUI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Zahin
+ */
+
+import javax.swing.JOptionPane;
+
+public class testGUI {
+    public static void main(String[] args) {
+        
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+        
+        
+        
+    }
+}
